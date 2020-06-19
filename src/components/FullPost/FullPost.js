778ca5bbcc300bd95f06c8318aba9dc266b23f86
Kyle Chivers <chiverskyle@gmail.com>
2020-06-18 22:29:51 -0700

    added state

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -4,11 +4,16 @@ import axios from 'axios';
 import './FullPost.css';
 
 class FullPost extends Component {
+    state = {
+        loadedPost: null
+    }
+
     componentDidUpdate () {
         if (this.props.id) {
             axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
                 .then(response => {
-                    console.log(response)
+                    // console.log(response)
+                    this.setState({loadedPost: response.data});
                 });
         }
     }
@@ -18,8 +23,8 @@ class FullPost extends Component {
         if (this.props.id) {
             post = (
                 <div className="FullPost">
-                    <h1>Title</h1>
-                    <p>Content</p>
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
                         <button className="Delete">Delete</button>
                     </div>
